1359697f445e1992c4f47a07c78db2a914218a95
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-01 17:47:25 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -110,4 +110,22 @@ describe('DbAddAccount Usecase', () => {
     const accountPromise = sut.add(accountData);
     await expect(accountPromise).rejects.toThrow();
   });
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut();
+    
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email@mail.com",
+      password: "valid_password"
+    }
+
+    const account = await sut.add(accountData);
+    expect(account).toEqual(expect.objectContaining({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email@mail.com",
+      password: "hashed_password"
+    }));
+  });
 });
