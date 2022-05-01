9002c9830ec283fd349c574e51a3536d3d393953
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-01 11:39:23 -0300

    test: ensure SignUpController returns 201 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -214,4 +214,26 @@ describe("SignUp Controller", () => {
       password: "any_password",
     });
   });
+
+  test("Should return 201 if valid data is provided", () => {
+    const { sut } = makeSut();
+    
+    const httpRequest: HttpRequest = {
+      body: {
+        name: "valid_name",
+        email: "valid_email@mail.com",
+        password: "valid_password",
+        passwordConfirmation: "valid_password",
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(201);
+    expect(httpResponse.body).toEqual(expect.objectContaining({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email@mail.com",
+      password: "valid_password",
+    }));
+  });
 });
