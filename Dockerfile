ad9c8256400b857fb3b0bd43eb800a8ce6167fd1
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-03 16:12:27 -0300

    feat: connect to mongodb before app listen

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:14.18.1-alpine
+
+USER node
+
+WORKDIR /home/node/api
---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || "mongodb://mongo/clean-node-api",
+  port: process.env.PORT || 3000
+}
