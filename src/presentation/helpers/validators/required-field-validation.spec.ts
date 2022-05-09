62ea508b5e370efc16fa3aa0ee7d628d855832ac
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-09 20:11:05 -0300

    test: ensure RequiredFieldValidation not returns if validation success

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({});
     expect(error).toEqual(new MissingParamError("any_field"));
   });
+
+  it('Should not return if validation success ', () => {
+    const sut = new RequiredFieldValidation("any_field");
+    const error = sut.validate({ any_field: "any_value" });
+    expect(error).toBeFalsy();
+  });
 });
