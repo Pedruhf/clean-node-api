f00702010e14909c21ed34c9b8e177ea3f7bfd30
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-12 21:10:52 -0300

    refactor: move fakeAccountModel creation to a factory method

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -2,16 +2,19 @@ import { AccountModel } from "../../../domain/models/account";
 import { LoadAccountByEmailRepository } from "../../protocols/load-account-by-email-repository";
 import { DbAuthentication } from "./db-authentication";
 
+const makeFakeAccount = (): AccountModel => {
+  return {
+    id: "any_id",
+    name: "any_name",
+    email: "any_mail@mail.com",
+    password: "any_password",
+  };
+}
+
 const makeLoadAccountByEmailRepositoryStub = (): LoadAccountByEmailRepository => {
   class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
     async load (email: string): Promise<AccountModel> {
-      const account: AccountModel = {
-        id: "any_id",
-        name: "any_name",
-        email: "any_mail@mail.com",
-        password: "any_password",
-      }
-
+      const account = makeFakeAccount();
       return new Promise(resolve => resolve(account));
     }
   }
