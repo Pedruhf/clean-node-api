c6b66f55e941a350d2e47335c9f10562437e40b6
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-12 21:30:58 -0300

    test: ensure DbAccount returns null if LoadAccountByEmailRepository fails

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -63,4 +63,16 @@ describe('DbAuthentication UseCase', () => {
 
     await expect(accessTokenPromise).rejects.toThrow();
   });
+
+  it('Should return null if LoadAccountByEmailRepository fails', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+    jest.spyOn(loadAccountByEmailRepositoryStub, "load").mockReturnValueOnce(null);
+
+    const accessToken = await sut.auth({
+      email: "any_mail@mail.com",
+      password: "any_password",
+    });
+
+    expect(accessToken).toBe(null);
+  });
 });
