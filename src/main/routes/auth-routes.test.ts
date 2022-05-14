a621c1d89d35e11ed5c0d8cd0f1235dc779b78cb
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-14 19:21:11 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
 
-describe('SignUp Route', () => {
+describe('Auth Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -16,15 +16,17 @@ describe('SignUp Route', () => {
     await accountCollection.deleteMany({});
   });
   
-  it('Should return an account on success', async () => {
-    await request(app)
-      .post("/api/signup")
-      .send({
-        name: "Pedro Freitas",
-        email: "pedrofreitas@gmail.com",
-        password: "asdasdasd",
-        passwordConfirmation: "asdasdasd"
-      })
-      .expect(201);
+  describe('POST /signup', () => {
+    it('Should return 201 on signup success', async () => {
+      await request(app)
+        .post("/api/signup")
+        .send({
+          name: "Pedro Freitas",
+          email: "pedrofreitas@gmail.com",
+          password: "asdasdasd",
+          passwordConfirmation: "asdasdasd"
+        })
+        .expect(201);
+    });
   });
 });
