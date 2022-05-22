9649f4ac12b23d1236e445662799b70676de20fd
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-05-22 18:43:56 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { Validation } from "../../../../presentation/protocols/validation";
+import { ValidationComposite, RequiredFieldValidation } from "../../../../validation/validators";
+import { makeAddSurveyValidation } from "./add-survey-validation-factory";
+
+jest.mock("../../../../validation/validators/validation-composite");
+
+describe('AddSurveyValidation Factory', () => {
+  it('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation();
+    const validations: Validation[] = [];
+    for (const field of ["question", "answers"]) {
+      validations.push(new RequiredFieldValidation(field));
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations);
+  });
+});
